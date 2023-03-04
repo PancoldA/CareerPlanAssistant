acd1defbcb2da9306f0d9b1951fbf935d173cb3b
blackreo123 <blackreo123@naver.com>
2023-03-05 01:45:51 +0900

    show index

--- src/main/java/com/cpa/careerplanassistant/CareerPlanAssistantApplication.java
+++ src/main/java/com/cpa/careerplanassistant/CareerPlanAssistantApplication.java
@@ -2,12 +2,15 @@ package com.cpa.careerplanassistant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@RestController
 public class CareerPlanAssistantApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CareerPlanAssistantApplication.class, args);
     }
-
 }
