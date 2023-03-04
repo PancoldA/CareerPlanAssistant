07613ab1dbd095d1fafb224e50e48ae88662cfb7
blackreo123 <blackreo123@naver.com>
2023-03-05 00:38:49 +0900

    first commit

---
+++ src/main/java/com/cpa/careerplanassistant/CareerPlanAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.cpa.careerplanassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerPlanAssistantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CareerPlanAssistantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cpa/careerplanassistant/CareerPlanAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cpa.careerplanassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerPlanAssistantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
